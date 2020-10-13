a265a0316b215c946e227cfb2fdc825f573271f8
mrunciman <m.runciman@imperial.ac.uk>
2020-10-13 11:41:51 +0100

    Imported changes to arduinoInterface.py from master. Will have to alter controlSystem.py in both branches.

--- arduinoInterface.py
+++ arduinoInterface.py
@@ -7,116 +7,122 @@ protection and volume calculation.
 import serial 
 import time
 
-def connect(pumpName, portNumber):
-    """
-    Each pump requires verification of the cable it is contolling (top, lhs, or rhs).
-    Pumps will function normally when the pumpName sent here matches the name hardcoded 
-    on each arduino.
-    e.g. top = connect("TOP", 4)
-    """
-    message = pumpName + "\n"
-    reply = ""
-    port = 'COM%s' % (portNumber)
+class ardInterfacer:
 
-    #Open serial port at given COM port at 115200 baud rate
-    ser = serial.Serial(port = port, baudrate = 115200, timeout = 0) 
-    message = message.encode('utf-8')    #Encode message
-    time.sleep(1)     #give arduino time to set up (there are delays in arduino code for pressure sensor)
-    while(1):
-        time.sleep(0.5)     #delay before sending message again
-        ser.write(message)
-        # print("Handshake: ", message)
-        if ser.in_waiting > 0:
-            reply = ser.readline().strip()
-            ser.reset_input_buffer()
-            reply = reply.decode('ascii')
-
-            if reply == pumpName:
-                ser.reset_output_buffer()
-                break
-
-    # return open serial connection to allow pumps to be controlled in main code
-    return ser, reply
-
-
-
-def sendStep(ser, stepNumber):
-    """
-    This function sends ideal position (stepNumber) then receives
-    the real step count (stepCount) from arduino.
-    steps = sendStep(serialConnection, stepNumber)
-    """
-    message = "S" + str(stepNumber) + "\n"
-    # print("Message: ", message)
-    message = message.encode('utf-8')
-    ser.write(message)
-    return
-
-
-
-
-def listenReply(ser):
-    x = "e"
-    stepPress = b""
-    noBytes = 0
-    # Wait here for reply - source of delay
-    while noBytes == 0:
-        noBytes = ser.in_waiting
-    # Read all bytes in input buffer
-    # stepPress = ser.read(noBytes)
-    # Check for end character
-    while ord(x) != ord("E"):
-        x = ser.read()
-        if x == b"":
-            break
-        elif x == b"E":
-            break
-        stepPress = stepPress + x
-
-    stepPress = stepPress.decode('utf-8')
-    stepPress = stepPress.split(',')
-    # print(stepPress)
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-    if stepPress == ['']:
-        stepCount = "S_Empty" # Change this later to handle dropped values
-        pumpPress = "P_Empty"
-        pumpTime = "T_Empty"
-    else:
-        stepCount = stepPress[0]
-        pumpPress = float(stepPress[1])/10
-        pumpTime = stepPress[2]
-    return stepCount, pumpPress, pumpTime
-
-
-def listenZero(ser, pumpZero):
-    x = "e"
-    stepPress = b""
-    while ser.in_waiting == 0:
+    def __init__(self):
+        # No class attributes, to avoid overwriting data between methods
         pass
-    # Check for end character
-    while ord(x) != ord("E"):
-        x = ser.read()
-        if x == b"":
-            break
-        elif x == b"E":
-            break
-        stepPress = stepPress + x
+    
 
-    # ser.reset_input_buffer()
-    # ser.reset_output_buffer()
-    if stepPress == b"":
-        stepCount = "S_Empty" # Change this later to handle dropped values
-        pumpPress = "P_Empty"
-        pumpTime = "T_Empty"
-    else:
-        # print(stepPress)
+    def connect(self, pumpName, portNumber):
+        """
+        Each pump requires verification of the cable it is contolling (top, lhs, or rhs).
+        Pumps will function normally when the pumpName sent here matches the name hardcoded 
+        on each arduino.
+        e.g. top = connect("TOP", 4)
+        """
+        message = pumpName + "\n"
+        reply = ""
+        port = 'COM%s' % (portNumber)
+
+        #Open serial port at given COM port at 115200 baud rate
+        ser = serial.Serial(port = port, baudrate = 115200, timeout = 0) 
+        message = message.encode('utf-8')    #Encode message
+        time.sleep(1)     #give arduino time to set up (there are delays in arduino code for pressure sensor)
+        while(1):
+            time.sleep(0.5)     #delay before sending message again
+            ser.write(message)
+            # print("Handshake: ", message)
+            if ser.in_waiting > 0:
+                reply = ser.readline().strip()
+                ser.reset_input_buffer()
+                reply = reply.decode('ascii')
+
+                if reply == pumpName:
+                    ser.reset_output_buffer()
+                    break
+
+        # return open serial connection to allow pumps to be controlled in main code
+        return ser, reply
+
+
+
+    def sendStep(self, ser, stepNumber):
+        """
+        This function sends ideal position (stepNumber) then receives
+        the real step count (stepCount) from arduino.
+        steps = sendStep(serialConnection, stepNumber)
+        """
+        message = "S" + str(stepNumber) + "\n"
+        # print("Message: ", message)
+        message = message.encode('utf-8')
+        ser.write(message)
+        return
+
+
+
+    def listenReply(self, ser):
+        x = "e"
+        stepPress = b""
+        noBytes = 0
+        # Wait here for reply - source of delay
+        while noBytes == 0:
+            noBytes = ser.in_waiting
+        # Read all bytes in input buffer
+        # stepPress = ser.read(noBytes)
+        # Check for end character
+        while ord(x) != ord("E"):
+            x = ser.read()
+            if x == b"":
+                break
+            elif x == b"E":
+                break
+            stepPress = stepPress + x
+
         stepPress = stepPress.decode('utf-8')
         stepPress = stepPress.split(',')
-        stepCount = stepPress[0]
-        pumpPress = float(stepPress[1])/10
-        pumpTime = stepPress[2]
+        # print(stepPress)
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+        if stepPress == ['']:
+            stepCount = "S_Empty" # Change this later to handle dropped values
+            pumpPress = "P_Empty"
+            pumpTime = "T_Empty"
+        else:
+            stepCount = stepPress[0]
+            pumpPress = float(stepPress[1])/10
+            pumpTime = stepPress[2]
+        return stepCount, pumpPress, pumpTime
 
-    if (pumpZero == True):
-        stepCount = 0
-    return stepCount, pumpPress, pumpTime
+
+    def listenZero(self, ser, pumpZero):
+        x = "e"
+        stepPress = b""
+        while ser.in_waiting == 0:
+            pass
+        # Check for end character
+        while ord(x) != ord("E"):
+            x = ser.read()
+            if x == b"":
+                break
+            elif x == b"E":
+                break
+            stepPress = stepPress + x
+
+        # ser.reset_input_buffer()
+        # ser.reset_output_buffer()
+        if stepPress == b"":
+            stepCount = "S_Empty" # Change this later to handle dropped values
+            pumpPress = "P_Empty"
+            pumpTime = "T_Empty"
+        else:
+            # print(stepPress)
+            stepPress = stepPress.decode('utf-8')
+            stepPress = stepPress.split(',')
+            stepCount = stepPress[0]
+            pumpPress = float(stepPress[1])/10
+            pumpTime = stepPress[2]
+
+        if (pumpZero == True):
+            stepCount = 0
+        return stepCount, pumpPress, pumpTime
