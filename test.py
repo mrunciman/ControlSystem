7be61f4c0b35670d3ed384fe9fe0a9799d29d279
msrunciman <m.runciman@imperial.ac.uk>
2020-10-19 23:08:09 +0100

    Updated GUI with minimum cable restriction. Investigating if I can add pre generated path to GUI and update to show current position.

---
+++ test.py
@@ -0,0 +1,28 @@
+
+import csv
+
+pathCounter = 0
+cycleCounter = 0
+
+
+with open ("C:/Users/msrun/OneDrive - Imperial College London/Imperial/Fluidic Control/ControlSystem/paths/circCoords 2020-10-19.csv",\
+    mode = 'r', newline='') as pathFile:
+    coordList = csv.reader(pathFile, delimiter=',')
+    xCoordList = []
+    yCoordList = []
+    for row in coordList:
+        xCoord = row[0]
+        yCoord = row[1]
+        xCoordList.append(xCoord)
+        yCoordList.append(yCoord)
+
+while cycleCounter < 3:
+    print(xCoordList[pathCounter], yCoordList[pathCounter])
+    pathCounter += 1
+    if pathCounter >= len(xCoordList):
+        pathCounter = 0
+        cycleCounter += 1
+        print(cycleCounter)
+
+
+
